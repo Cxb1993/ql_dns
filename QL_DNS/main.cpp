069b483cecbb7065ad219e425bdcf659d11067b8
jonosquire <jsquire@Jonathans-MacBook-Pro-2.local>
2014-08-10 18:00:33 -0400

    Initial Commit

---
+++ QL_DNS/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  QL_DNS
+//
+//  Created by Jonathan Squire on 8/10/14.
+//  Copyright (c) 2014 J Squire. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
