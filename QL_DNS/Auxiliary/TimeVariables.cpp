9089984c73ceea0a8ddc6908f641d96fab54fcb8
jonosquire <jsquire@jonathans-mbp-2.home>
2014-08-25 16:55:47 -0400

    Added time variables class

---
+++ QL_DNS/Auxiliary/TimeVariables.cpp
@@ -0,0 +1,9 @@
+//
+//  TimeVariables.cpp
+//  QL_DNS
+//
+//  Created by Jonathan Squire on 8/25/14.
+//  Copyright (c) 2014 J Squire. All rights reserved.
+//
+
+#include "TimeVariables.h"
---
+++ QL_DNS/Auxiliary/TimeVariables.h
@@ -0,0 +1,14 @@
+//
+//  TimeVariables.h
+//  QL_DNS
+//
+//  Created by Jonathan Squire on 8/25/14.
+//  Copyright (c) 2014 J Squire. All rights reserved.
+//
+
+#ifndef __QL_DNS__TimeVariables__
+#define __QL_DNS__TimeVariables__
+
+#include <iostream>
+
+#endif /* defined(__QL_DNS__TimeVariables__) */
